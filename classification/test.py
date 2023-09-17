eed830cad80e17e042a4e4dd9417c47b7bea338c
JACazares <josecazares5305@gmail.com>
2023-09-17 01:25:16 -0400

    Add PDFs and texts

--- classification/test.py
+++ classification/test.py
@@ -23,43 +23,102 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 #     return {str(response['choices'][0]['message']['content']): str(text)}
 
-def ask_gpt(context, qry):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": """Imagine you are a very enthusiastic professor at a very prestigious university. You just gave a lecture,
-             and a student has a question for you. Answer in a helpful way, using content only from the lecture you just gave. If you don't know the answer,
-             do not invent things, simply say that you currently cannot help with the question, or that you don't know the answer.
-             The lecture you just gave is: """ + context + ". The question the student has is: " + qry + "."},
-            {"role": "user", "content": "Professor, I have a question about the lecture. My question is: " + qry},
-        ],
-        temperature=0.7
-    )
+# def ask_gpt(context, qry):
+#     response = openai.ChatCompletion.create(
+#         model="gpt-3.5-turbo",
+#         messages=[
+#             {"role": "system", "content": """Imagine you are a very enthusiastic professor at a very prestigious university. You just gave a lecture,
+#              and a student has a question for you. Answer in a helpful way, using content only from the lecture you just gave. If you don't know the answer,
+#              do not invent things, simply say that you currently cannot help with the question, or that you don't know the answer.
+#              The lecture you just gave is: """ + context + ". The question the student has is: " + qry + "."},
+#             {"role": "user", "content": "Professor, I have a question about the lecture. My question is: " + qry},
+#         ],
+#         temperature=0.7
+#     )
 
-    return response['choices'][0]['text']['message']['content']
+#     return response['choices'][0]['text']['message']['content']
+
+
+def get_txt(source):
+    from PyPDF2 import PdfReader
+
+    reader = PdfReader(source)
+    text = ""
+    for page in reader.pages:
+        text += page.extract_text()
+    
+    return text
+
+import csv
+
+def read_config():
+    configs = []
+    with open('config.csv', 'r') as file:
+        reader = csv.reader(file)
+        next(reader)  # Skip the header row
+        for row in reader:
+            configs.append(row)
+    return configs
+
+import requests
 
 if __name__ == "__main__":
-    with open('16.842 transcr.txt', 'r') as file:
-        contents = file.read().split()
-        contents = ' '.join(contents)
-        contents = re.split('\.|\!|\?', contents)
+    configs = read_config()
+    print(configs)
 
-        file.close()
+    for lec_name, lec_id, class_name, class_id, youtube_link, pdf_link in configs:
+        print(lec_name, lec_id)
+        response_text = get_txt(f'lecture_materials/{lec_name}.pdf')
+        with open(f'lecture_converted/{lec_name}.txt', 'w') as file:
+            file.write(response_text)    
 
-    i = 0
-    sz = 10
-    categories = {}
-    chunk = []
-    while i*sz < len(contents):
-        import requests
+            file.close()
 
-        base_url = "http://your-api-url.com"
+    for lec_name, lec_id, class_name, class_id, youtube_link, pdf_link in configs:
+        with open(f'lecture_converted/{lec_name}.txt', 'r') as file:
+            contents = file.read().split()
+            contents = ' '.join(contents)
+            contents = re.split('\.|\!|\?', contents)
 
+            file.close()
+        
+        # Add class
+        base_url = "https://mau-md--embeddings-app.modal.run/add-class"
         params = {
-            "lecture_id": 1,
-            "query": contents[i*sz:(i+1)*sz]
+            "class_name": class_name,
         }
+        response = requests.post(base_url, json=params)
+        class_id = response.text
 
-        response = requests.post(base_url, params=params)
+        print(lec_name, class_id, youtube_link, pdf_link)
 
-        i += sz - 2
+        # Add Lecture
+        base_url = "https://mau-md--embeddings-app.modal.run/add-lecture"
+        params = {
+            "lecture_name": lec_name,
+            "class_id": class_id,
+            "youtube_link": youtube_link,
+            "pdf_link": pdf_link
+        }
+        response = requests.post(base_url, json=params)
+        lec_id = response.text
+
+        i = 0
+        sz = 15
+        categories = {}
+        chunk = []
+        while i*sz < len(contents):
+
+            base_url = "https://mau-md--embeddings-app.modal.run/add-context"
+
+            params = {
+                "lecture_id": lec_id,
+                "query": ". ".join(contents[i*sz:(i+2)*sz])
+            }
+
+            # print(params)
+
+            response = requests.post(base_url, json=params)
+            print(response)
+
+            i += 1
