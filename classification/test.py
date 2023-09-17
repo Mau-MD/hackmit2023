a44da490478b821d91e2b45b3a8dc81239e99515
JACazares <josecazares5305@gmail.com>
2023-09-16 20:38:41 -0400

    test

--- classification/test.py
+++ classification/test.py
@@ -1,12 +1,62 @@
-import tiktoken
+import openai
+import os
+import re
+from dotenv import load_dotenv
+from multiprocessing import Process
 
-enc = tiktoken.get_encoding("cl100k_base")
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-assert enc.decode(enc.encode("hello world")) == "hello world"
+# def get_summary(text):
+#     response = openai.ChatCompletion.create(
+#         model="gpt-3.5-turbo",
+#         messages=[
+#             {"role": "system", "content": "You are a helpful assistant"},
+#             {"role": "user", "content": "I will give you a few sentences and you will return a small summary for those sentences"},
+#             {"role": "assistant", "content": "How should I give you an output?"},
+#             {"role": "user", "content": "As a string of text, without any headers, just the summary. Again, no headers, just the summary."},
+#             {"role": "assistant", "content": "Got it. So, I will give you a string of text."},
+#             {"role": "user", "content": "Yes. Now, this is the text: " + text}
+#         ],
+#         temperature=0.2
+#     )
 
-# To get the tokeniser corresponding to a specific model in the OpenAI API:
-enc = tiktoken.encoding_for_model("gpt-4")
+#     return {str(response['choices'][0]['message']['content']): str(text)}
 
-s = input()
+def ask_gpt(context, qry):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "Imagine you are a professor. This are some relevant parts of the lecture you just gave: " + context},
+            {"role": "user", "content": "Professor, I have a question on the lecture. My question is: " + qry},
+        ],
+        temperature=0.7
+    )
 
-print(enc.encode(s))
+    return response['choices'][0]['text']['message']['content']
+
+if __name__ == "__main__":
+    with open('16.842 transcr.txt', 'r') as file:
+        contents = file.read().split()
+        contents = ' '.join(contents)
+        contents = re.split('\.|\!|\?', contents)
+
+        file.close()
+
+    i = 0
+    sz = 10
+    categories = {}
+    chunk = []
+    while i*sz < len(contents):
+        import requests
+
+        base_url = "http://your-api-url.com"
+
+        params = {
+            "lecture_id": 1,
+            "query": contents[i*sz:(i+1)*sz]
+        }
+
+        response = requests.post(base_url, params=params)
+
+        i += sz - 2
--- testing/hello-app/goodbye.py
+++ testing/hello-app/goodbye.py
@@ -1,2 +1,5 @@
+from modal import Image, Stub, wsgi_app
+
+@wsgi_app()
 def testing(test_id):
     return "Testing " + test_id + "!"
